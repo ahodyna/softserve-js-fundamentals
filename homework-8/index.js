c1186f08fe8bd872f3c28563c2d797c9152599fc
alina <hodyna.alina@gmail.com>
2020-12-09 13:58:41 +0200

    homework 8: added 3b & 7b tasks

--- homework-8/index.js
+++ homework-8/index.js
@@ -12,28 +12,35 @@ upperCase('Regexp')
 upperCase('geRExp')
 
 // **************************task 2******************************
-function checkEmail(email){
-    if(/^.*$/.test(email)){
-        console.log('true')   
-    }else{
+function checkEmail(email) {
+    if (/^.*$/.test(email)) {
+        console.log('true')
+    } else {
         console.log('false')
     }
 }
 
 checkEmail("Qmail2@gmail.com")
 
-//**************************task 3*******************************
+//**************************task 3a*******************************
 
 $('.head').css('background-color', 'green');
 $('.inner').css('font-size', '35px')
 
+// ************************task 3b*******************************
+
+let str1 = "cdbBdbsbz";
+
+let resultArr = str1.match(/d(b+)(d)/i)
+
+console.log(resultArr)
 
 //**************************task 4******************************
 
 function checkSelectedInput() {
-  let  tagInputCheked = document.querySelectorAll('input:checked')
+    let tagInputCheked = document.querySelectorAll('input:checked')
     if (tagInputCheked.length == 3) {
-        document.querySelectorAll('input').forEach(elem => elem.disabled=true);
+        document.querySelectorAll('input').forEach(elem => elem.disabled = true);
     }
 }
 
@@ -58,15 +65,33 @@ function checkCreditCard(number) {
 
 checkCreditCard("9999-9999-9999-9999")
 
-//***************************task 7***************************
+//***************************task 7a***************************
 
-function checkEmailAddress(str){
-    if (/^[a-zA-Z0-9]+[\w]*-?[\w]*@[a-z]+\.[a-z]+$/.test(str)){
+function checkEmailAddress(str) {
+    if (/^[a-zA-Z0-9]+[\w]*-?[\w]*@[a-z]+\.[a-z]+$/.test(str)) {
         console.log('Email is correct!')
-    } else{
+    } else {
         console.log('Email is not correct!')
     }
 }
 
 checkEmailAddress('my_mail@gmail.com');
 checkEmailAddress('#my_mail@gmail.com');
+
+//*********************************task 7b************************
+
+function checkLogin(str) {
+    let validLogin = str.length > 2
+        && str.length < 10
+        && /^[a-zA-Z][a-zA-Z0-9.]+$/.test(str);
+    console.log(validLogin)
+
+    let numbersArr = str.match(/[\d.]+/g);
+    if (numbersArr != null) {
+        console.log(numbersArr.map(str => Number(str)))
+    }
+
+}
+
+checkLogin('ee1.1ret3');
+checkLogin('ee1*1ret3');
