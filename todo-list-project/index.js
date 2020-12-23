0c198a190a4a26b30c9790163d6080f4cb3c448f
alina <hodyna.alina@gmail.com>
2020-12-23 17:36:43 +0200

    added coments for Todo list presentation & style for popup

--- todo-list-project/index.js
+++ todo-list-project/index.js
@@ -1,3 +1,4 @@
+// printed Todo List from Local Storage
 window.addEventListener('load', function () {
     printListMenu();
 
@@ -6,10 +7,9 @@ window.addEventListener('load', function () {
     }
 })
 
-// added list Menu
+// added list Menu(section menu)
 let listMenuInput = document.getElementById('listMenuInput');
 let addListMenuButton = document.getElementById('addListMenuButton');
-
 
 listMenuInput.addEventListener("keyup", function (event) {
 
@@ -24,7 +24,7 @@ addListMenuButton.addEventListener('click', function () {
 
     if (listMenuInput.value == "") {
         document.getElementById("listMenuEmptyInputPopup").classList.add('show')
-        
+
     } else {
 
         TodoDataStorage.addTodo(new TodoList(listMenuInput.value, []))
@@ -34,67 +34,6 @@ addListMenuButton.addEventListener('click', function () {
         printTodoDetails(TodoDataStorage.todoAmount() - 1)
     }
 });
-
-function createButtonComponent(id, className, text, clickActionCallback) {
-
-    let buttonNode = document.createElement('button')
-    buttonNode.className = className
-    buttonNode.id = id
-    buttonNode.textContent = text
-    buttonNode.addEventListener('click', clickActionCallback)
-
-    return buttonNode;
-}
-
-function createPopupComponent(id, text) {
-
-    let popupNode = document.createElement('div')
-    popupNode.className = 'popup'
-
-    let popupSpanNode = document.createElement('span')
-    popupSpanNode.id = id
-    popupSpanNode.textContent = text
-    popupSpanNode.className = 'popuptext'
-    popupNode.appendChild(popupSpanNode);
-
-    return popupNode;
-}
-
-
-function printTodoDetails(listIndex) {
-
-    console.log('printTodoDetails: ', listIndex)
-    const todoItemCreationSection = document.getElementById('todoItemCreationSection');
-    todoItemCreationSection.innerHTML = ""
-
-    const deleteButtonSection = document.getElementById('deleteButtonSection');
-    deleteButtonSection.innerHTML = "";
-
-    const listNameSection = document.getElementById('listNameSection');
-    listNameSection.innerHTML = "";
-
-    if (listIndex != null) {
-        let componentPopup = createPopupComponent('listDstailsEmptyInputPopup', 'Please fill item name');
-        todoItemCreationSection.appendChild(componentPopup)
-      
-        const addItemInputNode = document.createElement('input')
-        addItemInputNode.addEventListener('keyup', (event) => onkeyupAddTodoItemInput(event, listIndex))
-        addItemInputNode.id = 'addItemInput'
-        addItemInputNode.className = 'inputForm'
-        addItemInputNode.setAttribute('placeholder', 'Add item')
-        todoItemCreationSection.appendChild(addItemInputNode);    
-    
-        const addItemButtonNode = createButtonComponent('buttonAddItem', 'addButton', 'Add', () => addTodoItem(listIndex));
-        todoItemCreationSection.appendChild(addItemButtonNode);
-
-        const deleteListButtonNode = createButtonComponent('buttonDeleteItem', 'deleteList', 'Delete list', () => deleteTodoList(listIndex));
-        deleteButtonSection.appendChild(deleteListButtonNode);
-
-        listNameSection.innerHTML = TodoDataStorage.getListTitleByIndex(listIndex);
-    }
-
-    printList(listIndex)
-}
 
 function printListMenu() {
 
@@ -111,7 +50,68 @@ function printListMenu() {
 
 };
 
-// added List Details
+// Button for section menu
+function createButtonComponent(id, className, text, clickActionCallback) {
+
+    let buttonNode = document.createElement('button')
+    buttonNode.className = className
+    buttonNode.id = id
+    buttonNode.textContent = text
+    buttonNode.addEventListener('click', clickActionCallback)
+
+    return buttonNode;
+}
+
+// Popup for section menu input
+function createPopupComponent(id, text) {
+
+    let popupNode = document.createElement('div')
+    popupNode.className = 'popup'
+
+    let popupSpanNode = document.createElement('span')
+    popupSpanNode.id = id
+    popupSpanNode.textContent = text
+    popupSpanNode.className = 'popuptext'
+    popupNode.appendChild(popupSpanNode);
+
+    return popupNode;
+}
+
+//implemented section content
+function printTodoDetails(listIndex) {
+    const todoItemCreationSection = document.getElementById('todoItemCreationSection');
+    todoItemCreationSection.innerHTML = ""
+
+    const deleteButtonSection = document.getElementById('deleteButtonSection');
+    deleteButtonSection.innerHTML = "";
+
+    const listNameSection = document.getElementById('listNameSection');
+    listNameSection.innerHTML = "";
+
+    if (listIndex != null) {
+        let componentPopup = createPopupComponent('listDstailsEmptyInputPopup', 'Please fill item name');
+        todoItemCreationSection.appendChild(componentPopup)
+
+        const addItemInputNode = document.createElement('input')
+        addItemInputNode.addEventListener('keyup', (event) => onkeyupAddTodoItemInput(event, listIndex))
+        addItemInputNode.id = 'addItemInput'
+        addItemInputNode.className = 'inputForm'
+        addItemInputNode.setAttribute('placeholder', 'Add item')
+        todoItemCreationSection.appendChild(addItemInputNode);
+
+        const addItemButtonNode = createButtonComponent('buttonAddItem', 'addButton', 'Add', () => addTodoItem(listIndex));
+        todoItemCreationSection.appendChild(addItemButtonNode);
+
+        const deleteListButtonNode = createButtonComponent('buttonDeleteItem', 'deleteList', 'Delete list', () => openPopup(listIndex));
+        deleteButtonSection.appendChild(deleteListButtonNode);
+
+        listNameSection.innerHTML = TodoDataStorage.getListTitleByIndex(listIndex);
+    }
+
+    printList(listIndex)
+}
+
+// Section content (print list item)
 
 function printList(listIndex) {
 
@@ -138,31 +138,23 @@ function printList(listIndex) {
 };
 
 function onkeyupAddTodoItemInput(event, todoIndex) {
-    console.log('onkeyupAddTodoItemInput: ', todoIndex)
+
     if (event.key === 'Enter') {
         addTodoItem(todoIndex);
-    }else {
+    } else {
         document.getElementById("listDstailsEmptyInputPopup").classList.remove('show')
     }
 };
 
-function deleteTodoList(index) {
-    TodoDataStorage.deleteTodoListByIndex(index);
-    printListMenu()
-    printTodoDetails(TodoDataStorage.todoAmount() > 0 ? 0 : null)
-};
-
 function addTodoItem(todoIndex) {
 
-    console.log('add Todo Item: ', todoIndex, TodoDataStorage.getListTitleByIndex(todoIndex))
     let inputFieldForItem = document.getElementById('addItemInput');
 
     if (inputFieldForItem.value == "") {
         document.getElementById("listDstailsEmptyInputPopup").classList.add('show')
-        
+
     } else {
         TodoDataStorage.addTodoItemByIndex(todoIndex, new TodoItem(false, inputFieldForItem.value))
-        console.log('going to print list. todoIndex: ', todoIndex);
         printList(todoIndex);
         inputFieldForItem.value = "";
     }
@@ -176,4 +168,27 @@ function deleteElem(todoIndex, indexItem) {
 function checkItem(todoIndex, indexItem) {
     TodoDataStorage.checkTodoItemByIndexes(todoIndex, indexItem);
     printList(todoIndex);
+};
+
+// Button delete Todo List & Popup elements
+function openPopup(index) {
+    const modalBoxNode = document.getElementById('modalBox');
+    modalBoxNode.style.display = 'block';
+    modalBoxNode.setAttribute('listIndexToDelete', index)
+};
+
+function closePopup() {
+    document.getElementById('modalBox').style.display = 'none';
+};
+
+
+function deleteTodoList() {
+    const modalBoxNode = document.getElementById('modalBox');
+    const listIndexToDeleteStr = modalBoxNode.getAttribute('listIndexToDelete')
+
+    TodoDataStorage.deleteTodoListByIndex(Number(listIndexToDeleteStr));
+    printListMenu()
+    printTodoDetails(TodoDataStorage.todoAmount() > 0 ? 0 : null)
+
+    closePopup()
 };
